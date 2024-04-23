5c1bfd0b05c3a0128c15e358a7c54657d5b39a6b
James O'Rourke <devbopo.joe@gmail.com>
2024-04-23 15:36:03 +0100

    Update isit.js

--- isit.js
+++ isit.js
@@ -24,7 +24,7 @@ var missedLunch = m.getTime();
 
 if (current > almostLunch && current < startLunch) {
   document.write('<h1 class="almost">Almost</h1>');
-  document.write(almostLunch-current);
+  document.write(current-almostLunch);
 } else if (current > endLunch && current < missedLunch) {
   document.write('<h1 class="missed">Just missed it</h1>');
 } else if (current < startLunch || current > endLunch) {
