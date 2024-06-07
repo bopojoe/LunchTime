e3842264dd5b38480bcfb4add596279d5dd5432f
James O'Rourke <devbopo.joe@gmail.com>
2024-06-07 15:44:28 +0100

    Update isit.js

--- isit.js
+++ isit.js
@@ -1,36 +1,55 @@
-var now = new Date();
+function updateMessage() {
+  var now = new Date();
 
-var a = new Date();
-a.setHours(15);
-a.setMinutes(30);
+  var a = new Date();
+  a.setHours(15);
+  a.setMinutes(30);
 
-var s = new Date();
-s.setHours(16);
-s.setMinutes(00);
+  var s = new Date();
+  s.setHours(16);
+  s.setMinutes(0);
 
-var e = new Date();
-e.setHours(17);
-e.setMinutes(0);
+  var e = new Date();
+  e.setHours(17);
+  e.setMinutes(0);
 
-var m = new Date();
-m.setHours(17);
-m.setMinutes(01);
+  var m = new Date();
+  m.setHours(17);
+  m.setMinutes(1);
 
-var current = now.getTime();
-var almostLunch = a.getTime();
-var startLunch = s.getTime();
-var endLunch = e.getTime();
-var missedLunch = m.getTime();
-var difference = (startLunch-current) / 1000;
-var value = difference/60;
+  var current = now.getTime();
+  var almostLunch = a.getTime();
+  var startLunch = s.getTime();
+  var endLunch = e.getTime();
+  var missedLunch = m.getTime();
+  var difference = (startLunch - current) / 1000;
+  var value = difference / 60;
 
-if (current > almostLunch && current < startLunch) {
-  document.write('<h1 class="almost">Almost</h1>');
-  document.write(`<h1 class="almost">${value} minutes</h1>`);
-} else if (current > endLunch && current < missedLunch) {
-  document.write('<h1 class="missed">Just missed it</h1>');
-} else if (current < startLunch || current > endLunch) {
-  document.write("<h1>No</h1>");
-} else {
-  document.write('<h1 class="yes">Yes</h1>');
+  var messageElement = document.getElementById('message');
+  var timeLeftElement = document.getElementById('time-left');
+
+  if (current > almostLunch && current < startLunch) {
+    messageElement.className = 'almost';
+    messageElement.textContent = 'Almost';
+    timeLeftElement.className = 'almost';
+    timeLeftElement.textContent = `${value.toFixed(2)} minutes`;
+  } else if (current > endLunch && current < missedLunch) {
+    messageElement.className = 'missed';
+    messageElement.textContent = 'Just missed it';
+    timeLeftElement.textContent = '';
+  } else if (current < startLunch || current > endLunch) {
+    messageElement.className = 'no';
+    messageElement.textContent = 'No';
+    timeLeftElement.textContent = '';
+  } else {
+    messageElement.className = 'yes';
+    messageElement.textContent = 'Yes';
+    timeLeftElement.textContent = '';
+  }
 }
+
+// Update the message every second
+setInterval(updateMessage, 1000);
+
+// Initial call to set the message immediately on page load
+updateMessage();
